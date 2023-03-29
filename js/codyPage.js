82ebaf1dbc4840bcbe65b97a60bb22f84f1949ae
정종영 <115339701+jungjongyeong@users.noreply.github.com>
2023-03-29 15:28:49 +0900

    Add files via upload

--- js/codyPage.js
+++ js/codyPage.js
@@ -1,26 +1,42 @@
 
-    const h = document.querySelector('#h');
-    const b = document.body;
-    
-    let base = (e) => {
-        var x = e.pageX / window.innerWidth - 0.5;
-        var y = e.pageY / window.innerHeight - 0.5;
-        h.style.transform = `
+const h = document.querySelector('#h');
+const b = document.body;
+
+let base = (e) => {
+    var x = e.pageX / window.innerWidth - 0.5;
+    var y = e.pageY / window.innerHeight - 0.5;
+    h.style.transform = `
             perspective(90vw)
             rotateX(${y * 4 + 75}deg)
             rotateZ(${-x * 12 + 45}deg)
             translateZ(-9vw)
         `;
-    }
-    
-    b.addEventListener('pointermove', base);
-    
-    $('.house .puerta_open').mouseover(function () {
-        $('.stats').css({'opacity':'1', 'z-index':'9999'})
-        $('.info').css({'opacity':'0'})
-    })
-    $('.house .puerta_open').mouseout(function () {
-        $('.stats').css({'opacity':'0', 'z-index':'-1'})
-        $('.info').css({'opacity':'1'})
-    })
+}
+
+b.addEventListener('pointermove', base);
+
+$('.house .puerta_open').mouseover(function () {
+    $('.stats').css({ 'opacity': '1', 'z-index': '9999' })
+    $('.icon').css({ 'opacity': '0' })
+    $('.main_title').css({ 'opacity': '0' })
+})
+$('.house .puerta_open').mouseout(function () {
+    $('.stats').css({ 'opacity': '0', 'z-index': '-1' })
+    $('.icon').css({ 'opacity': '1' })
+    $('.main_title').css({ 'opacity': '1' })
+})
+
+const $icon = document.querySelector('.icon');
+const $arrow = document.querySelector('.arrow');
+
+$arrow.animate([
+    { top: '0' },
+    { top: '10px' },
+    { top: '0' }
+], {
+    duration: 700,
+    iterations: Infinity
+});
+
+
 
--- js/travel.js
+++ js/travel.js
@@ -18,40 +18,9 @@
         } else if (12<=month || month<=2) {
             season = 'winter'
         }
-        // console.log(season)
+        console.log(season)
         // console.log(local)
 
-        // 버튼별 지역명을 클래스로 붙이기
-        $('button#seoul').on('click', function(){
-            $('button').removeClass()
-            $(this).addClass('on')
-            $('#contBox').removeClass('busan gangwon daejeon gwangju jeju').addClass('seoul')
-        })
-        $('button#busan').on('click', function(){
-            $('button').removeClass()
-            $(this).addClass('on')
-            $('#contBox').removeClass('seoul gangwon daejeon gwangju jeju').addClass('busan')
-        })
-        $('button#gangwon').on('click', function(){
-            $('button').removeClass()
-            $(this).addClass('on')
-            $('#contBox').removeClass('seoul busan daejeon gwangju jeju').addClass('gangwon')
-        })
-        $('button#daejeon').on('click', function(){
-            $('button').removeClass()
-            $(this).addClass('on')
-            $('#contBox').removeClass('seoul busan gangwon gwangju jeju').addClass('daejeon')
-        })
-        $('button#gwangju').on('click', function(){
-            $('button').removeClass()
-            $(this).addClass('on')
-            $('#contBox').removeClass('seoul busan gangwon daejeon jeju').addClass('gwangju')
-        })
-        $('button#jeju').on('click', function(){
-            $('button').removeClass()
-            $(this).addClass('on')
-            $('#contBox').removeClass('seoul busan gangwon daejeon gwangju').addClass('jeju')
-        })
 
         // 월별 계절 변경
         if (season == 'spring') {
@@ -63,18 +32,113 @@
         } else if (season == 'winter') {
             $('#contBox').addClass('winter')
         }
-
-        // 넘어온 지역을 근교 별로 전환
-/*         if (areaKoreanName = 'seoul.kr') {
-            local = 'seoul'
-        }
-        if (areaKoreanName = 'Incheon,KR'){
-            local = 'seoul'
-        } */
         
     }
-        
+    // 버튼별 지역명을 클래스로 붙이기
+    $('button#seoul').on('click', function(){
+        $('#map button').removeClass()
+        $('#map .outTitle').addClass('off')
+        $('#contBox .inTitle').empty().append(`<i class="fa-solid fa-car-side"></i> 오늘은 <span>서울 근교</span>로 떠나볼까? <i class="fa-solid fa-car-side"></i>`)
+        $(this).addClass('on')
+        $('#contBox').removeClass('close busan gangwon daejeon gwangju jeju').addClass('seoul')
+    })
+    $('button#busan').on('click', function(){
+        $('#map button').removeClass()
+        $('#map .outTitle').addClass('off')
+        $('#contBox .inTitle').empty().append(`<i class="fa-solid fa-car-side"></i> 오늘은 <span>부산 근교</span>로 떠나볼까? <i class="fa-solid fa-car-side"></i>`)
+        $(this).addClass('on')
+        $('#contBox').removeClass('close seoul gangwon daejeon gwangju jeju').addClass('busan')
+    })
+    $('button#gangwon').on('click', function(){
+        $('#map button').removeClass()
+        $('#map .outTitle').addClass('off')
+        $('#contBox .inTitle').empty().append(`<i class="fa-solid fa-car-side"></i> 오늘은 <span>강원도</span>로 떠나볼까? <i class="fa-solid fa-car-side"></i>`)
+        $(this).addClass('on')
+        $('#contBox').removeClass('close seoul busan daejeon gwangju jeju').addClass('gangwon')
+    })
+    $('button#daejeon').on('click', function(){
+        $('#map button').removeClass()
+        $('#map .outTitle').addClass('off')
+        $('#contBox .inTitle').empty().append(`<i class="fa-solid fa-car-side"></i> 오늘은 <span>대전 근교</span>로 떠나볼까? <i class="fa-solid fa-car-side"></i>`)
+        $(this).addClass('on')
+        $('#contBox').removeClass('close seoul busan gangwon gwangju jeju').addClass('daejeon')
+    })
+    $('button#gwangju').on('click', function(){
+        $('#map button').removeClass()
+        $('#map .outTitle').addClass('off')
+        $('#contBox .inTitle').empty().append(`<i class="fa-solid fa-car-side"></i> 오늘은 <span>광주 근교</span>로 떠나볼까? <i class="fa-solid fa-car-side"></i>`)
+        $(this).addClass('on')
+        $('#contBox').removeClass('close seoul busan gangwon daejeon jeju').addClass('gwangju')
+    })
+    $('button#jeju').on('click', function(){
+        $('#map button').removeClass()
+        $('#map .outTitle').addClass('off')
+        $('#contBox .inTitle').empty().append(`<i class="fa-solid fa-car-side"></i> 오늘은 <span>제주도</span>로 떠나볼까? <i class="fa-solid fa-car-side"></i>`)
+        $(this).addClass('on')
+        $('#contBox').removeClass('close seoul busan gangwon daejeon gwangju').addClass('jeju')
+    })
 
+    // 모바일창 닫기버튼
+    $('button#close').on('click', function(){
+        $('#contBox').removeClass('seoul busan gangwon daejeon gwangju jeju').addClass('close')
+        $('button').removeClass('on')
+        $('#map .outTitle').removeClass('off')
+    })
+
+    $('#contBox.close').on('click', function(){
+        $('#contBox .inTitle').empty().append('<i class="fa-solid fa-car-side"></i> 오늘은 어디로 떠나볼까? <i class="fa-solid fa-car-side"></i>')
+        
+    })
+
+    // 지도 3d로 움직이게 하기
+    const frame = document.getElementById('map')
+      const card = document.getElementById('map')
+      const light = document.getElementById('map')
+
+      let { x, y, width, height } = frame.getBoundingClientRect()
+
+      function mouseMove(e) {
+        const left = e.clientX - x
+        const top = e.clientY - y
+        const centerX = left - width / 2
+        const centerY = top - height / 2
+        const d = Math.sqrt(centerX**2 + centerY**2)
+
+        // card.style.boxShadow = `
+        //   ${-centerX / 5}px ${-centerY / 10}px 10px rgba(0, 0, 0, 0.2)
+        // `
+
+        card.style.transform = `
+          rotate3d(
+            ${-centerY / 100}, ${centerX / 100}, 0, ${d / 8}deg
+          )
+        `
+
+        // light.style.backgroundImage = `
+        //   radial-gradient(
+        //     circle at ${left}px ${top}px, #00000040, #ffffff00, #ffffff99
+        //   )
+        // `
+      }
+
+      frame.addEventListener('mouseenter', () => {
+        frame.addEventListener('mousemove', mouseMove)
+      })
+
+      frame.addEventListener('mouseleave', () => {
+        frame.removeEventListener('mousemove', mouseMove)
+        card.style.boxShadow = ''
+        card.style.transform = ''
+        light.style.backgroundImage = ''
+      })
+
+      window.addEventListener('resize', () => {
+        rect = frame.getBoundingClientRect()
+        x = rect.x
+        y = rect.y
+        width = rect.width
+        height = rect.height
+      })
         
 })(jQuery)
 
